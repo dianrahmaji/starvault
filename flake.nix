e24eb3c05c5c8a14b2d413ec2caa878b36a5c6cf
Dian Rahmaji <dianrahmaji@gmail.com>
2025-03-08 15:13:52 +0700

    feat(nix): configure flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+	description = "knowledge management project's flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		utils.url = "github:numtide/flake-utils";
+  	};
+
+	outputs = { self, nixpkgs, utils }:
+		utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = (import (nixpkgs) { inherit system; }); 
+			in {
+				devShell = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						nest-cli
+						nodejs_22
+						pnpm
+					];
+				};
+			}
+		);
+}
