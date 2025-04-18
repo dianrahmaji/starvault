1a20d9d574c5c2219098bf84529c5859452b72dd
Dian Rahmaji <dianrahmaji@gmail.com>
2025-04-18 20:14:01 +0700

    feat: configure `bullmq`

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
@@ -8,7 +9,16 @@ import config from './mikro-orm.config';
 
 @Module({
 	controllers: [AppController],
-	imports: [ConfigModule.forRoot(), MikroOrmModule.forRoot(config)],
+	imports: [
+		ConfigModule.forRoot(),
+		MikroOrmModule.forRoot(config),
+		BullModule.forRoot({
+			connection: {
+				host: process.env.REDIS_HOST,
+				port: parseInt(process.env.REDIS_PORT!),
+			},
+		}),
+	],
 	providers: [AppService],
 })
 export class AppModule {}
