ae59c884a806ca402e197baf68c2e24707b6cf6b
Dian Rahmaji <dianrahmaji.jakmall@gmail.com>
2026-05-31 21:18:39 +0700

    feat: register `idn` module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdnModule } from './idn/idn.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), IdnModule],
   controllers: [AppController],
   providers: [AppService],
 })
