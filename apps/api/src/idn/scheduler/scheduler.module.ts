aa3d41bf633993189d28c974d12233b596814b29
Dian Rahmaji <dianrahmaji.jakmall@gmail.com>
2026-06-01 21:25:57 +0700

    feat: add `getLiveStreams` scheduler

--- apps/api/src/idn/scheduler/scheduler.module.ts
+++ apps/api/src/idn/scheduler/scheduler.module.ts
@@ -1,7 +1,9 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
 
 @Module({
+  imports: [HttpModule],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
--- apps/api/src/idn/scheduler/scheduler.service.ts
+++ apps/api/src/idn/scheduler/scheduler.service.ts
@@ -1,4 +1,68 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { firstValueFrom } from 'rxjs';
+
+type Creator = {
+  name: string;
+  uuid: string;
+};
+
+type LiveStream = {
+  title: string;
+  image_url: string;
+  playback_url: string;
+  status: string;
+  creator: Creator;
+};
+
+type GraphqlResponse = {
+  data: {
+    getLivestreams: LiveStream[];
+  };
+};
+
+const IDN_GRAPHQL_URL = 'https://api.idn.app/graphql';
+const GET_LIVESTREAMS_QUERY = `
+  query($page: Int) {
+    getLivestreams(category: "all", page: $page) {
+      title
+      image_url
+      playback_url
+      status
+      creator {
+        name
+        uuid
+      }
+    }
+  }
+`;
 
 @Injectable()
-export class SchedulerService {}
+export class SchedulerService {
+  constructor(private readonly httpService: HttpService) {}
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async getLiveStreams() {
+    const allLiveStreams: LiveStream[] = [];
+
+    for (let page = 1; ; page++) {
+      const res = await firstValueFrom(
+        this.httpService.post<GraphqlResponse>(IDN_GRAPHQL_URL, {
+          query: GET_LIVESTREAMS_QUERY,
+          variables: {
+            page,
+          },
+        }),
+      );
+
+      const liveStreams = res.data.data.getLivestreams;
+
+      if (!liveStreams.length) {
+        break;
+      }
+
+      allLiveStreams.push(...liveStreams);
+    }
+  }
+}
