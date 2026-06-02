0f4b9529f881c1518af6e10c6e87b4c445fcadbc
Dian Rahmaji <dianrahmaji.jakmall@gmail.com>
2026-06-02 22:55:23 +0700

    feat: connect to database with `typeorm`

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdnModule } from './idn/idn.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), IdnModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'username',
+      password: 'password',
+      database: 'starvault',
+      entities: [],
+    }),
+    IdnModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
